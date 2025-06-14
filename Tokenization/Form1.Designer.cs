c4c5f4e798187a5c4ec66c724747c7ef1279952f
Muhammad-Saaad <210232835+UMAMA-ABBASI-05@users.noreply.github.com>
2025-06-14 09:26:58 +0500

    change the form design, add the input featres and working on the print [output feature]

--- Tokenization/Form1.Designer.cs
+++ Tokenization/Form1.Designer.cs
@@ -39,25 +39,28 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(98, 74);
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(98, 53);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(300, 112);
+            this.richTextBox1.Size = new System.Drawing.Size(520, 267);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(98, 274);
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(98, 392);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(300, 118);
+            this.richTextBox2.Size = new System.Drawing.Size(520, 207);
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(205, 192);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(261, 326);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 29);
+            this.button1.Size = new System.Drawing.Size(106, 29);
             this.button1.TabIndex = 2;
             this.button1.Text = "Compile";
             this.button1.UseVisualStyleBackColor = true;
@@ -65,11 +68,12 @@
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(562, 74);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(885, 74);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(152, 308);
+            this.listBox1.Size = new System.Drawing.Size(152, 504);
             this.listBox1.TabIndex = 3;
             // 
             // label1
@@ -86,7 +90,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(95, 231);
+            this.label2.Location = new System.Drawing.Point(94, 348);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 20);
             this.label2.TabIndex = 5;
@@ -95,17 +99,19 @@
             // lbValid
             // 
             this.lbValid.AutoSize = true;
-            this.lbValid.Location = new System.Drawing.Point(159, 395);
+            this.lbValid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbValid.Location = new System.Drawing.Point(279, 602);
             this.lbValid.Name = "lbValid";
-            this.lbValid.Size = new System.Drawing.Size(0, 16);
+            this.lbValid.Size = new System.Drawing.Size(58, 20);
             this.lbValid.TabIndex = 6;
+            this.lbValid.Text = "label 1";
             this.lbValid.UseMnemonic = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1035, 556);
+            this.ClientSize = new System.Drawing.Size(1317, 649);
             this.Controls.Add(this.lbValid);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
--- Tokenization/Form1.cs
+++ Tokenization/Form1.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Tokenization
@@ -17,32 +21,51 @@ namespace Tokenization
         public Form1()
         {
             InitializeComponent();
-            
         }
 
-        public Dictionary<String, String> dtype_var;
+        public Dictionary<String, String> dtype_var; 
         public Dictionary<String, String> var_value;
 
-        public void add_word(String first_word, String datatype)
+        public bool add_word(String first_word, String datatype) // first_word = "a = 10 | a" here you must have a data type
         {
-            if (first_word.Contains("=")) // means inilization
+            if (first_word.Contains("=")) // means inilization "num a = 10"
             {
                 var variable = first_word.Split('=')[0].Trim(); // variable name
                 var value = first_word.Split('=')[1].Trim(); // variable value
+
+                if (dtype_var.ContainsKey(variable)) // if the variable already exists
+                {
+                    lbValid.Text = "Build Failed"; // set the label to build failed
+                    MessageBox.Show($"Variable {variable} already exists: ");
+                    return false;
+                }
 
                 dtype_var[variable] = datatype; // add to dtype_var dictionary
                 var_value[variable] = value; // add to var_value
 
                 listBox1.Items.Add(variable);
+                listBox1.Items.Add("="); // add equal sign
                 listBox1.Items.Add(value);
+
+                return true;
             }
-            else // only deleration
-            {
+            else // only deleration "num a" 
+            { 
                 var variable = first_word.Trim(); // variable name
+
+                if (dtype_var.ContainsKey(variable)) // if the variable already exists
+                {
+                    lbValid.Text = "Build Failed"; // set the label to build failed
+                    MessageBox.Show($"Variable {variable} already exists: ");
+                    return false;
+                }
+
                 dtype_var[variable] = datatype; // add to dtype_var dictionary
                 var_value[variable] = null; // add to memory with null value
 
                 listBox1.Items.Add(variable);
+
+                return true;
             }
         }
 
@@ -60,13 +83,20 @@ namespace Tokenization
             {
                 var sub_words = string.Join(" ", words[0].Split(' ').Skip(1)); // got the first value
 
-                add_word(sub_words, dtype);
+                if(!add_word(sub_words, dtype))
+                {
+                    listBox1.Items.RemoveAt(listBox1.Items.Count - 1); // if the variable already exists then remove the data type from listbox
+                }
 
             }
             else // Multiple words
             {
                 var sub_words = string.Join(" ", words[0].Split(' ').Skip(1));
-                add_word(sub_words, dtype);
+                
+                if (!add_word(sub_words, dtype))
+                {
+                    listBox1.Items.RemoveAt(listBox1.Items.Count - 1); // if the variable already exists then remove the data type from listbox
+                }
 
                 foreach (var word in words.Skip(1))
                 {
@@ -75,63 +105,72 @@ namespace Tokenization
             }
         }
 
-        public void draft_regex()
+        public  Boolean type_validation(String variable, String value) // used only when inilization "a=10;"
         {
-            //float declaration 
-            //Regex FloatdLine = new Regex("^float [a-zA-Z][a-zA-Z0-9]*[' ']*;$");
+            try
+            {
+                if (dtype_var[variable] == "num")
+                {
+                    // get the data type form data_var dictionary if its integer then insert
+                    if (Regex.IsMatch(value, @"^\d+$")) // if it is integer
+                    {
+                        var_value[variable] = value;
+                        return true;
+                    }
+                }
+                else if (dtype_var[variable] == "float" || dtype_var[variable] == "decimal")
+                {
+                    if (Regex.IsMatch(value, @"^[0-9]*[0-9]*[.]?[0-9][0-9]*$"))
+                    {
+                        var_value[variable] = value;
+                        return true;
+                    }
+                }
+                else if (dtype_var[variable] == "text")
+                {
+                    if (Regex.IsMatch(value, @"^""[^""]*""$"))
+                    {
+                        var_value[variable] = value;
+                        return true;
+                    }
+                }
+                else if (dtype_var[variable] == "char")
+                {
+                    if (Regex.IsMatch(value, @"^'[^""]'$"))
+                    {
+                        var_value[variable] = value;
+                        return true;
+                    }
+                }
+                else if (dtype_var[variable] == "bool")
+                {
+                    if (Regex.IsMatch(value, @"^(true|false)$"))
+                    {
+                        var_value[variable] = value;
+                        return true;
+                    }
+                }
 
-            //float  declaration multiple 
-            //Regex FloatdMLine = new Regex("^int [a-zA-Z][a-zA-Z0-9]*[,][a-zA-Z][a-zA-Z0-9]*[' ']*[,' 'a-zA - Z0 - 9']*;$"); 
+                MessageBox.Show("Type Mismatch:  " + value);
+                return false;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return false;
+        }
 
-            //int declaration 
-            //if (IntdLine.IsMatch(line))
-            //{
-            //    String[] sub = words[0].Split(',');
+        public string get_input(string input_str)
+        {
+            var extractedText = input_str.Split('"')[1];
+            string userInput = Microsoft.VisualBasic.Interaction.InputBox(
+            extractedText,              // Message
+            "Input Required",           // Title
+            "",                         // Default text in input
+            -1, -1);                    // X and Y position (-1 for center)
 
-            //    foreach (string s in sub)
-            //    {
-            //        if (s == "")
-            //        {
-            //            break;
-            //        }
-            //        listBox1.Items.Add(s + "\n");
-            //    }
-            //    listBox1.Items.Add(words[1].Trim(new Char[] { ';' }) + "\n");
-            //}
-
-            //float declaration
-            //if (FloatdLine.IsMatch(line))
-            //{
-            //    listBox1.Items.Add(words[0] + "\n"); listBox1.Items.Add(words[1].Trim(new Char[] { ';' }) + "\n");
-            //}
-
-            // declaration single
-            //^(num | decimal | float | bool | char | text)\s + [_a - zA - Z][_a - zA - Z0 - 9] * [' '] *;$
-
-            ////for Multiple declaration 
-            //if (IntdMLine.IsMatch(line))
-            //{
-            //    string[] last = words[words.Length - 1].Split(';');
-            //    foreach (var w in words[0].Split(','))
-            //    {
-            //        string[] Iwords = w.Split('='); 
-            //        //listBox1.Items.Add(Iwords[0].Trim(new Char[] { ';' }) + "\n");
-            //        listBox1.Items.Add(Iwords[0] + "\n");
-            //        listBox1.Items.Add("=" + Iwords[1]);
-            //    }
-            //    listBox1.Items.Add(last[0] + "\n");
-            //}
-
-            //for Multiple declaration 
-            //if (FloatdMLine.IsMatch(line))
-            //{
-            //    listBox1.Items.Add(words[0] + "\n");
-            //    foreach (var w in words[1].Split(','))
-            //    {
-            //        string[] Iwords = w.Split('='); listBox1.Items.Add(Iwords[0].Trim(new Char[] { ';' }) + "\n");
-            //    }
-            //}
-
+            return userInput;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -169,17 +208,19 @@ namespace Tokenization
 
 
             //"print string" 
-            Regex printSLine = new Regex(@"^print\s*\(\s*""[a-zA-Z0-9' '\=][a-zA-Z0-9' '\=]*""\s*\)\s*;$");
+            Regex printSLine = new Regex(@"^print\s*\(\s*(?:""[^""]*""|\w+||\([^()]*\))(?:\s*[\+\-\*/%]\s*(?:""[^""]*""|\w+|\([^()]*\)))*\s*\)\s*;$");
 
             //"input string"
-            Regex inputLine = new Regex(@"^[_A-z][_A-z0-9]*\s*=\s*input\s*\(\s*""[\w\s]*""\s*\)\s*;$");
+            Regex inputLine = new Regex(@"^[_A-z][_A-z0-9]*\s*=\s*input\s*\(\s*""[\w\s=:]*""\s*\)\s*;$");
 
             string[] Input = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
             richTextBox2.Text = "";
 
-            foreach (var line in Input)
+            foreach (var line_ in Input)
             {
                 // decimal a, b1=1.2,b2=4.2;
+
+                var line = line_.Trim(); // line_ can have a space after ";" so i remove it here
 
                 // for initialization and declaration of any data type
 
@@ -188,46 +229,110 @@ namespace Tokenization
                 {
                     add_memory(line); // add to memory
 
-                    lbValid.Text = "Build Sucessfull";
+                    if (lbValid.Text != "Build Failed") // if the build is not failed then set the label to build successfull
+                    {
+                        lbValid.Text = "Build Sucessfull";
+                    }
+                    else // build failed stop the process
+                    {
+                        break;
+                    }
                 }
 
                 else if (ILine.IsMatch(line)) // Inilization all 
                 {
 
-                    string[] Iwords = line.Split('='); 
-                    listBox1.Items.Add(Iwords[0].Trim() + "\n");
+                    string[] Iwords = line.Split('=');
 
-                    listBox1.Items.Add("=" + "\n");
+                    // checking if the variable exists or not, if yes then check the type of input and variable
 
-                    listBox1.Items.Add(Iwords[1].Trim(new Char[] { ';' }) + "\n");
+                    var variable = Iwords[0].Trim();
 
-                    lbValid.Text = "Build Sucessfull";
+                    var value = Iwords[1].Trim(new Char[] { ';' }).Trim();
+
+                    if (type_validation(variable, value) == true){
+                        listBox1.Items.Add(variable + "\n"); // variable added
+
+                        listBox1.Items.Add("=" + "\n"); // equal added
+
+                        listBox1.Items.Add(value + "\n"); // value added
+
+                        lbValid.Text = "Build Sucessfull";
+                    }
+                    else
+                    {
+                        lbValid.Text = "Build Failed";
+                    }
+                    
 
                 }
                 else if (printSLine.IsMatch(line)) // print 
                 {
-                    string[] coutwords = line.Split('"'); 
 
-                    listBox1.Items.Add("print" + "\n");
+                    foreach (var stmt in line)
+                    {
+                        var inner = ExtractPrintContent(stmt);
+                        var output = EvaluateExpression(inner, var_value);
+                        Console.WriteLine(output);
+                    }
 
-                    listBox1.Items.Add(coutwords[1].Trim() + "\n");
+                    //string[] coutwords = line.Split('"'); 
 
-                    lbValid.Text = "Build Sucessfull";
-                    
+                    //listBox1.Items.Add("print" + "\n");
+
+                    //listBox1.Items.Add(coutwords[1].Trim() + "\n");
+
+                    //lbValid.Text = "Build Sucessfull";
+
                 }
 
-                else if (inputLine.IsMatch(line)) // input
+                else if (inputLine.IsMatch(line)) // input issue!
                 {
+                    bool got_inp = false;
                     var words = line.Split('=');
-                    listBox1.Items.Add(words[0].Trim() + "\n");
 
-                    Input inp = new Input();
-                    
-                    
+                    if (words.Length > 2)
+                    {
+                        for (int i =2; i < words.Length; i++)
+                        {
+                            words[1] = words[1] + "=" +words[i];
+                        }
+                    }
 
-                    listBox1.Items.Add(words[1].Trim(';').Trim(' '));
+                    foreach (var variable in dtype_var.Keys)
+                    {
+                        if (variable == words[0].Trim()) // check is the variable exist or not
+                        {
+                            // if exists then check its type and then assign the value accordingly
 
-                    lbValid.Text = "Build Sucessfull";
+                            if (dtype_var[variable] == "num" || dtype_var[variable] == "float" || dtype_var[variable] == "decimal" ||
+                                dtype_var[variable] == "char" || dtype_var[variable] == "bool")
+                            {
+                                String userInput = get_input(words[1]);
+
+                                if (userInput == "") // if no input is entered then return error
+                                {
+                                    lbValid.Text = "Build Failed";
+                                    break;
+                                }
+
+                                if (type_validation(variable, userInput) == true) //  check if the user input is correct and then add input
+                                {
+                                    listBox1.Items.Add(userInput);
+                                    got_inp = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    if (got_inp == true)
+                    {
+                        lbValid.Text = "Build Sucessfull";
+                    }
+                    else
+                    {
+                        lbValid.Text = "Build Failed";
+                    }
                 }
                 else
                 {
@@ -235,12 +340,96 @@ namespace Tokenization
                     break;
                 }
             }
+
+            if (lbValid.Text == "Build Sucessfull")
+            {
+                lbValid.ForeColor = Color.DarkGreen;
+            }
+            else
+            {
+                lbValid.ForeColor = Color.DarkRed;
+            }
+
             lbValid.Visible = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        // 1) grab what's inside print(...)
+        static string ExtractPrintContent(string s)
+        {
+            var m = Regex.Match(s, @"^print\s*\(\s*(.*)\s*\)\s*;$");
+            return m.Success ? m.Groups[1].Value : "";
+        }
+
+        // 2) split top‑level on '+' only (depth==0)
+        static List<string> SplitOnTopLevelPlus(string expr)
+        {
+            var parts = new List<string>();
+            int depth = 0, last = 0;
+            for (int i = 0; i < expr.Length; i++)
+            {
+                char c = expr[i];
+                if (c == '(') depth++;
+                else if (c == ')') depth--;
+                else if (c == '+' && depth == 0)
+                {
+                    parts.Add(expr.Substring(last, i - last));
+                    last = i + 1;
+                }
+            }
+            parts.Add(expr.Substring(last));
+            return parts;
+        }
+
+        // 3) evaluate each segment, then concatenate results
+        static string EvaluateExpression(string expr, Dictionary<string, object> vars)
+        {
+            expr = expr.Trim();
+            if (string.IsNullOrEmpty(expr))
+                return "";
+
+            //expr = expr.Replace("\\", "/");
+
+            // replace variables with their values (strings get quoted)
+            foreach (var kvp in vars)
+            {
+                string pat = $@"\b{kvp.Key}\b";
+                string val = kvp.Value is string ? $"\"{kvp.Value}\"" : kvp.Value.ToString();
+                expr = Regex.Replace(expr, pat, val);
+            }
+
+            var segments = SplitOnTopLevelPlus(expr);
+            string result = "";
+
+            foreach (var seg in segments)
+            {
+                var t = seg.Trim();
+                // is it a double‑quoted string?
+                if (t.StartsWith("\"") && t.EndsWith("\""))
+                {
+                    result += t.Substring(1, t.Length - 2);
+                }
+                else
+                {
+                    // otherwise treat as math and compute
+                    var table = new DataTable();
+                    try
+                    {
+                        var val = table.Compute(t, "");
+                        result += val.ToString();
+                    }
+                    catch
+                    {
+                        result += "[Error]";
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
